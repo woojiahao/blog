0dce4ca2605239c4aeb1fb444ee3b64a02995420
Jiahao, Woo <woojiahao1234@gmail.com>
2025-04-14 09:38:27 +0800

    Add page content or link to external article

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -12,7 +12,7 @@ export async function GET(context) {
       .map((post) => ({
         title: post.data.title,
         pubDate: post.data.date,
-        description: post.data.description,
+        description: post.data.url != null ? `This post was published on ${post.data.url}` : post.body,
         link: `/post/${post.id}`
       })),
     trailingSlash: false,
